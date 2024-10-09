3b6665e827d5d73ac0796f5e343087bafb323291
Darek Krawczyk <Darek9Krawczyk@hotmail.com>
2024-10-10 01:00:22 +0200

    Readme

---
+++ Server/Server/Doc/TODO.md
@@ -0,0 +1,11 @@
+﻿# TODO
+* [ ] Database architecture
+	* [ ] Keep track of each module in project	
+* [ ] Add models with controllers
+	* [x] User
+	* [x] Category
+	* [x] Purchase
+	* [x] PurchaseItem
+	* [x] Supplier
+	* [ ] Component
+	* [ ] ComponentProjects
---
+++ Server/Server/Models/Component.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models
+{
+    public class Component
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ComponentID { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Category))]
+        public int CategoryID { get; set; }
+        public Category? Category { get; set; }
+
+        public ICollection<PurchaseItem> PurchaseItems { get; set; } = new List<PurchaseItem>();
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Manufacturer { get; set; }
+
+        [Column(TypeName = "nvarchar(255)")]
+        public string Description { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Location { get; set; }
+    }
+}
